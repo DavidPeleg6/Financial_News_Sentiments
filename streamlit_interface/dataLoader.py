ef2c24e518e1e860dd1d938a68181f8c9a64cbd8
AvihaiDidi <avihai.didi@gmail.com>
2023-03-12 19:36:49 +0200

    implemented cache wrapper and used it on existing functions

--- streamlit_interface/dataLoader.py
+++ streamlit_interface/dataLoader.py
@@ -7,15 +7,35 @@ import datetime
 import os
 import boto3
 
+# This is the "don't waste money" variable, it makes the code load local data instead of accessing AWS
+# Set this to false to waste money (and also test the actual thing)
+_OFFLINE_DATA = True
 
 _update_interval = datetime.timedelta(days=1)
 
 _recommended_stocks_cache_filename = "recommended_stocks_cache"
 _past_accuracy_cache_filename = "past_accuracy_cache"
+_sentiment_data_cache_filename = "sentiment_data_cache"
 
 _datapth = 'temp_data/'
 _datetime_format = "%Y-%m-%d %H:%M:%S"
 _valid_predgoals = ['Daily', 'Weekly', 'Monthly']
+
+def cache_dec(filename: str):
+    # this decorator MUST be used alongside @st.cache_data. this one should come first
+    # the function which is decorated should have a 'time' variable, preferabbly with a default value of 'None'.
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if "time" not in kwargs:
+                print(f"Invalid use of the cache_dec decorator. {func} must have a time variable")
+            cachedata_fname = filename
+            if "predgoal" in kwargs:
+                cachedata_fname = cachedata_fname + "_" + kwargs["predgoal"]
+            time = _checkIfCacheUpdate(cachedata_fname)
+            result = func(*args, time = time, **kwargs)
+            return result
+        return wrapper
+    return decorator
 
 def _checkIfCacheUpdate(filename : str) -> datetime.datetime:
     """
@@ -41,26 +61,15 @@ def _checkIfCacheUpdate(filename : str) -> datetime.datetime:
     # elapsed time is smaller than _update_interval. Keep cache the same
     return last
 
-
-def getRecommendedStocks(predgoal : str = 'Weekly') -> pd.DataFrame:
-    """
-    gets as input a string detailing prediction targer, must be in the set _valid_predgoals
-    returns a pandas dataframe structured as follows:
-    company name, ticker, model predictions, past accuracy for this stock
-    
-    if less than a day has passed since the last call to this function, the cached result will be used
-    """
-    if predgoal not in _valid_predgoals:
-        return None
-    time = _checkIfCacheUpdate(_recommended_stocks_cache_filename + "_" + predgoal)
-    return _RecommendedStocksCache(predgoal, time)
-
-
-def getSentimentData() -> pd.DataFrame:
+@cache_dec(_sentiment_data_cache_filename)
+@st.cache_data
+def getSentimentData(time : datetime.datetime = None) -> pd.DataFrame:
     """
     returns a pandas dataframe structured as follows:
     company name, ticker, sentiment score, sentiment magnitude, sentiment score change, sentiment magnitude change
     """
+    if _OFFLINE_DATA:
+        return pd.read_csv("news_sentiments.csv", index_col="Date")
     # specify key and secret key
     aws_access_key_id = os.environ['DB_ACCESS_KEY']
     aws_secret_access_key = os.environ['DB_SECRET_KEY']
@@ -79,9 +88,18 @@ def getSentimentData() -> pd.DataFrame:
     sentiment_ticker_list = sentiment_ticker_list.set_index('Date').sort_index(ascending=False)
     return sentiment_ticker_list
 
-
+@cache_dec(_recommended_stocks_cache_filename)
 @st.cache_data
-def _RecommendedStocksCache(predgoal : str, time : datetime.datetime) -> pd.DataFrame:
+def getRecommendedStocks(predgoal : str = 'Weekly', time : datetime.datetime = None) -> pd.DataFrame:
+    """
+    gets as input a string detailing prediction targer, must be in the set _valid_predgoals
+    returns a pandas dataframe structured as follows:
+    company name, ticker, model predictions, past accuracy for this stock
+    
+    if less than a day has passed since the last call to this function, the cached result will be used
+    """
+    if predgoal not in _valid_predgoals:
+        return None
     # TODO: actually aquire data from the model, right now it just returns this constant thing
     # (I took this from https://www.nasdaq.com/, it's just the top 5 I sawthere)
     # this if statement should actually do different stuff based on predgoal
@@ -91,19 +109,15 @@ def _RecommendedStocksCache(predgoal : str, time : datetime.datetime) -> pd.Data
         return pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)), _datapth, 'weekly_recommendations.csv'))
     return pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)), _datapth, 'monthly_recommendations.csv'))
 
-
-def getPastAccuracy(time_back: int = 100) -> pd.DataFrame:
+@cache_dec(_past_accuracy_cache_filename)
+@st.cache_data
+def getPastAccuracy(time_back: int = 100, time : datetime.datetime = None) -> pd.DataFrame:
     """
     returns a pandas dataframe listing the accuracy of the model in the past for each type of prediction
     the input is how many days back from right now you want it to show
     
     if less than a day has passed since the last call to this function, the cached result will be used
     """
-    time = _checkIfCacheUpdate(_past_accuracy_cache_filename)
-    # TODO: when this thing actually returns a real value, use 'time_back' to truncate it
-    return _PastAccuracyCache(time)
-
-@st.cache_data
-def _PastAccuracyCache(time : datetime.datetime) -> pd.DataFrame:
     # TODO: actually do this, right now it just returns a randomly generated list
+    # TODO: when this thing actually returns a real value, use 'time_back' to truncate it
     return pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)), _datapth, 'past_accuracy.csv'))
