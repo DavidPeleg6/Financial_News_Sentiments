2326260257b161fd3aaad38d53da79cbd20be1d3
davidpeleg6 <davidpeleg6@gmail.com>
2023-03-20 17:44:57 +0000

    small tweak from container

--- AWS_codebase/price_scrape.py
+++ AWS_codebase/price_scrape.py
@@ -57,7 +57,7 @@ def get_stockprice(company_symbol: str = 'MSFT'):
         if response.status_code == 200 and 'Note' not in response.json():
             return response.json()
         else:
-            # logger.info(f'API key {parameters["apikey"]} has been used too many times. response note: {response.json()}')
+            logger.info(f'API key {parameters["apikey"]} has been used too many times.')
             time.sleep(1)
 
 
