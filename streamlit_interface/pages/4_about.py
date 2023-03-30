515150d9f3783e4f70813e85b100225db7a6f5a2
AvihaiDidi <avihai.didi@gmail.com>
2023-03-30 13:35:29 +0300

    capitalize A

--- streamlit_interface/pages/4_about.py
+++ streamlit_interface/pages/4_about.py
@@ -5,8 +5,8 @@ dudu_github = "https://github.com/DavidPeleg6"
 avihai_linkedin = "https://www.linkedin.com/in/avihai-didi-ba47a6224/"
 avihai_github = "https://github.com/AvihaiDidi"
 
-st.header('Stock analasys and recomendation platform')
-st.subheader('Developed by David Peleg and Avihai Didi, 2023')
+st.header("Stock analasys and recomendation platfor")
+st.subheader("Developed by David Peleg and Avihai Didi, 2023")
 
 st.write(f"David Peleg [github]({dudu_github}), [linkedin]({dudu_linkedin})")
 st.write(f"Avihai Didi [github]({avihai_github}), [linkedin]({avihai_linkedin})")
