44160e826d03ea280ee6b5acd0281a786ecfa13e
AvihaiDidi <avihai.didi@gmail.com>
2023-04-06 18:58:33 +0300

    renamed this lambda function

--- AWS_codebase/model_get_predictions.py
+++ AWS_codebase/model_get_predictions.py
@@ -1,20 +1,26 @@
-"""
-This file is just a collection of all of the lambda functions used in aws for model related functionality
-Each function is preceded by all of the imports that it has on aws, even if it already appears elsewhere in this file
-Each is also encased in these big lines to signify the points between them
-"""
-# ----------------------------------------------------------------------------------------------------------
+import json, boto3, pymysql
+import pandas as pd
+import datetime, io, os
+from boto3.dynamodb.conditions import Key, Attr
+import traceback
+
+table_name = "ModelsXGB"
+
+# incosistent column names bandaid fix
+colnames_dict = {
+    "adj_close": "adjusted_close",
+    "dividend": "dividend_amount",
+    "split": "split_coefficient"
+}
+
 def lambda_handler(event, context):
     """
     Gets a token name and a range of dates as input
     Returns the predictions for those dates as a dictionary
 
     """
-    import json, boto3, pymysql
-    import pandas as pd
-    import datetime, io, os
-    from boto3.dynamodb.conditions import Key, Attr
     import xgboost as xgb
+    import sklearn
     # parse input
     token = event.get('token', '')
     start_s = event.get('start', '')
@@ -28,41 +34,57 @@ def lambda_handler(event, context):
         print("No data avilable for " + token)
         return {
             'statusCode': 500,
-            'body': stock_prices.to_json()
+            'body': "No data avilable for " + token
         }
     
     # get the model
-    model = get_model(token)
+    model = get_model_ddb(token)
     if model == None:
         print("No model avilable for " + token)
         return {
             'statusCode': 500,
-            'body': stock_prices.to_json()
+            'body': "No model avilable for " + token
         }
     # make predictions for the data using the model
-    pred = model.predict(stock_prices.drop(['close']))
+    try:
+        # rename columns (this is coz we were inconsistent in naming these)
+        stock_prices = stock_prices.rename(columns=colnames_dict)
+        pred = model.predict(stock_prices.drop('close', axis=1))
+    except Exception as e:
+        print("Failed to make predictions.")
+        print(str(e))
+        return {
+            'statusCode': 500,
+            'body': "Prediction failed for " + token
+        }
+    # the predictions are a numpy array, so we put them in a dataframe
+    pred_df = pd.DataFrame({'Date': stock_prices.index, 'close': pred})
     return {
             'statusCode': 200,
-            'body': pred.to_json()
+            'body': pred_df.to_json()
         }
 
-bucket_name = "financialnewssentimentsmodel"
-folder_name = "model_data/"
-
-def get_model(token: str) -> xgb.XGBRegressor:
+def get_model_ddb(token: str):
     # returns a model that can be used to predict the value of 'token'
     # if model loading fails it returns None
-    s3 = boto3.client('s3')
-    key = folder_name + token + ".bin"
-    try:
-        s3_object = s3.get_object(Bucket=bucket_name, Key=key)
-        model_bytes = s3_object['Body'].read()
-        # Load the saved XGBoost model from binary data
-        model = xgb.Booster(model_file=io.BytesIO(model_bytes))
-    except Exception as e:
-        print("Error:\t"+ str(e))
+    import xgboost as xgb
+    import pickle
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+    table = dynamodb.Table("ModelsXGB")
+    response = table.query(KeyConditionExpression=Key('Stock').eq(token))
+    if 'Items' not in response:
         return None
-    return model
+    try:
+        item = response['Items'][0]
+        # Retrieve the binary model data from the 'model_data' attribute
+        model_data = item['Model'].value
+        # Load the binary data into an XGBoost model using pickle
+        xgb_model = pickle.loads(model_data)
+    except Exception as e:
+        print("Error when attempting to parse model bytes")
+        print(str(e))
+        return None
+    return xgb_model
 
 def get_data(token: str, start: datetime.date, end: datetime.date) -> pd.DataFrame:
     # loads and returns data from an RDS database for the date range given
@@ -77,13 +99,10 @@ def get_data(token: str, start: datetime.date, end: datetime.date) -> pd.DataFra
             db  =  os.environ['rds_db']
             )
         # Execute SQL query
-        query_old = f"""SELECT *
-                FROM Prices
-                WHERE Date BETWEEN '{start_s}' AND '{end_s}'
-                AND Stock = '{str.upper(token)}';"""
         query = f"""SELECT *
                 FROM Prices
-                WHERE Stock = '{str.upper(token)}';"""
+                WHERE Date BETWEEN '{start_s}' AND '{end_s}'
+                AND Stock = "{str.upper(token)}";"""
         data = pd.read_sql_query(query, connection, parse_dates=['Date'])
         connection.close()
         # convert the data to a pandas dataframe and drop the stock column
@@ -95,5 +114,3 @@ def get_data(token: str, start: datetime.date, end: datetime.date) -> pd.DataFra
         print("Error:\t" + str(e))
         df = pd.DataFrame()
     return df
-
-# ----------------------------------------------------------------------------------------------------------
